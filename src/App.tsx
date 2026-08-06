ab26f6f981fa2bc1a8b504b85974cc38bfee5a16
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-08-06 15:56:23 +0300

    feat: add component Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PersonDetail from "./components/PersonDetail/PersonDetail";
 import { AboutPage } from "./components/AboutPage/AboutPage";
-
+import Footer from "./components/Footer/Footer";
 
 function App() {
  return (
   <BrowserRouter>
-   <Header />
-   <Routes>
-    <Route path="/" element={<CategorySelect />} />
-    <Route path="/persons/:slug" element={<PersonDetail />} />
-    <Route path="/about" element={<AboutPage/>}/>
-   </Routes>
+   <div className="app">
+    <Header />
+    <main className="main">
+      <Routes>
+     <Route path="/" element={<CategorySelect />} />
+     <Route path="/persons/:slug" element={<PersonDetail />} />
+     <Route path="/about" element={<AboutPage />} />
+    </Routes>
+    </main>
+    <Footer />
+   </div>
   </BrowserRouter>
  );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer-container container">
+        <Link to="/">На главную</Link>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
